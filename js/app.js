77cc34530e2a5e7be239df193e6567d721b125a8
vedthegreat <vedthegreat2004@gmail.com>
2023-09-09 01:07:46 +0530

    i have some idea now YSGIUDCHSLXJDRKFS

--- js/app.js
+++ js/app.js
@@ -1,62 +1,101 @@
-class Box extends React.Component{
-    state={color:'black'}
-    changeColor(color){
-        this.setState({color:color});
-    }
-    render(){
-        return(
-            <div className='box'>
-                <h1 className={this.state.color}>{this.props.heading}</h1>
-                <p>GIHDRXLDIFKJSMLCDKFSDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD</p>
-                <button onClick={()=>this.changeColor("red")}>Red</button>
-                <button onClick={()=>this.changeColor("yellow")}>Yellow</button>
-                <button onClick={()=>this.changeColor("blue")}>Blue</button>
-                <button onClick={()=>this.changeColor("green")}>Green</button>
-            </div>
-        );
-    }
+const Box = props => {
+    return(
+        <div className='box'>
+           <h1 className={props.color}>{props.heading}</h1>
+            <p>GIHDRXLDIFKJSMLCDKFSDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD</p>
+            <button onClick={()=>props.changeColor(props.id,"red")}>Red</button>
+            <button onClick={()=>props.changeColor(props.id,"yellow")}>Yellow</button>
+            <button onClick={()=>props.changeColor(props.id,"blue")}>Blue</button>
+            <button onClick={()=>props.changeColor(props.id,"green")}>Green</button>
+        </div>
+    );
+}
+const Stats = (props) => {
+        let boxes=props.boxes;
+        let red=0,yellow=0,blue=0,green=0,black=0;
+        boxes.forEach(box => {
+        if(box.color=='red'){
+            red++;
+        }
+        else if(box.color=='yellow'){
+            yellow++;
+        }
+        else if(box.color=='blue'){
+            blue++;
+        }
+        else if(box.color=='green'){
+            green++;
+        }
+        else{
+            black++;
+        }
+    });
+    return(
+        <div className='stats'>
+            Total heading color counts:
+            <div>Black:{black}</div>
+            <div>Red:{red}</div>
+            <div>Yellow:{yellow}</div>
+            <div>Blue:{blue}</div>
+            <div>Green:{green}</div>
+        </div>
+    );
 };
 class App extends React.Component{
     state={
         boxes:[
             {
+                id:1,
                 heading:'first heading',
                 color:'black'
             },
             {
+                id:2,
                 heading:'second heading',
                 color:'black'
             },
             {
+                id:3,
                 heading:'third heading',
                 color:'black'
             },
             {
+                id:4,
                 heading:'fourth heading',
                 color:'black'
             },
             {
+                id:5,
                 heading:'fifth heading',
                 color:'black'
             },
             {
+                id:6,
                 heading:'sixth heading',
-                color:'black'
-            },
-            {
-                heading:'seventh heading',
                 color:'black'
             }
         ]
     }
+    changeColor(id,color){
+        let boxes=this.state.boxes;
+        boxes[id-1].color=color;
+        this.setState({
+            boxes:boxes
+        });
+    }
     render(){
         return(
+        <div>
         <div className='row'>
             {this.state.boxes.map(box =>
                 <div className='col'>
-                    <Box heading={box.heading} color={box.color}/>
+                    <Box id={box.id} heading={box.heading} color={box.color} changeColor={this.changeColor.bind(this)}/>
                 </div>
             )}
+        </div>
+        <div className='row'>
+            <Stats boxes={this.state.boxes}/>
+        </div>
         </div>
         );
     }
