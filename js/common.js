547c6468da6da8e0f9b0bb0f6418833cf40c3a5c
vedthegreat <vedthegreat2004@gmail.com>
2023-09-13 02:33:56 +0530

    a

--- js/common.js
+++ js/common.js
@@ -3,6 +3,7 @@ const loginLink=document.querySelector(".login-link");
 const registerLink=document.querySelector(".register-link");
 const btnPopup=document.querySelector(".btnLogin-popup");
 const iconClose=document.querySelector(".icon-close");
+const Alert=document.querySelector(".Alertbox");
 
 registerLink.addEventListener("click",()=>{
     wrapper.classList.add("active");
@@ -15,4 +16,7 @@ btnPopup.addEventListener("click",()=>{
 });
 iconClose.addEventListener("click",()=>{
     wrapper.classList.remove("active-popup");
+});
+Alert.addEventListener("error",()=>{
+    Alert.classList.remove("active-error");
 });
