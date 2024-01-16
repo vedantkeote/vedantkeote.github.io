c558d1de7ba627fc7def0af2ff9213b292c56e80
vedthegreat <vedthegreat2004@gmail.com>
2024-01-16 18:29:44 +0530

    updates

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menu=document.querySelector('.menulinks');
+    const icon=document.querySelector('.burger-icon');
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
+}
